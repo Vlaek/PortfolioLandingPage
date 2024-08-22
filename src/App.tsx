514e6dfa409f9c58b7f492cf9a461a9ed2d22908
Vladislav Eichwald <vlad.eichwald@gmail.com>
2024-08-22 14:55:14 +0700

    fix modal bug

--- src/App.tsx
+++ src/App.tsx
@@ -13,51 +13,51 @@ import { ISkill } from './components/Skill/skill.interface'
 import ExperienceSection from './components/ExperienceSection/ExperienceSection'
 
 const App: FC = () => {
-	const [projectModalIsOpen, setProjectModalIsOpen] = useState<boolean>(false)
-	const [skillModalIsOpen, setSkillModalIsOpen] = useState<boolean>(false)
-	const [project, setProject] = useState<IProject | null>(null)
-	const [skill, setSkill] = useState<ISkill | null>(null)
+  const [projectModalIsOpen, setProjectModalIsOpen] = useState<boolean>(false)
+  const [skillModalIsOpen, setSkillModalIsOpen] = useState<boolean>(false)
+  const [project, setProject] = useState<IProject | null>(null)
+  const [skill, setSkill] = useState<ISkill | null>(null)
 
-	function openProjectModal(newProject: IProject) {
-		setProjectModalIsOpen(true)
-		setProject(newProject)
-	}
+  function openProjectModal(newProject: IProject) {
+    setProjectModalIsOpen(true)
+    setProject(newProject)
+  }
 
-	function openSkillModal(newSkill: ISkill) {
-		setSkillModalIsOpen(true)
-		setSkill(newSkill)
-	}
+  function openSkillModal(newSkill: ISkill) {
+    setSkillModalIsOpen(true)
+    setSkill(newSkill)
+  }
 
-	return (
-		<>
-			<Nav />
-			<Header />
-			<Section id={'skills'} title={'These Are My Skills'} color={'#818ba4'}>
-				{skills.map((item, index) => (
-					<Skill key={index} data={item} openModal={openSkillModal} />
-				))}
-			</Section>
-			<Section id={'works'} title={'These Are My Projects'} color={'#333'}>
-				{projects.map((item, index) => (
-					<Project key={index} project={item} openModal={openProjectModal} />
-				))}
-			</Section>
-			<Section id={'about'} title={'About Me'} color={'#818ba4'}>
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Section id={'skills'} title={'These Are My Skills'} color={'#818ba4'}>
+        {skills.map((item, index) => (
+          <Skill key={index} data={item} openModal={openSkillModal} />
+        ))}
+      </Section>
+      <Section id={'works'} title={'These Are My Projects'} color={'#333'}>
+        {projects.map((item, index) => (
+          <Project key={index} project={item} openModal={openProjectModal} />
+        ))}
+      </Section>
+      <Section id={'about'} title={'About Me'} color={'#818ba4'}>
         <ExperienceSection />
-			</Section>
-			<Footer />
-			<ProjectModal
-				project={project}
-				modalIsOpen={projectModalIsOpen}
-				setModalIsOpen={setProjectModalIsOpen}
-			/>
-			<SkillModal
-				skill={skill}
-				modalIsOpen={skillModalIsOpen}
-				setModalIsOpen={setSkillModalIsOpen}
-			/>
-		</>
-	)
+      </Section>
+      <Footer />
+      <ProjectModal
+        project={project}
+        modalIsOpen={projectModalIsOpen}
+        setModalIsOpen={setProjectModalIsOpen}
+      />
+      <SkillModal
+        skill={skill}
+        modalIsOpen={skillModalIsOpen}
+        setModalIsOpen={setSkillModalIsOpen}
+      />
+    </>
+  )
 }
 
 export default App
--- src/components/SkillModal/SkillModal.tsx
+++ src/components/SkillModal/SkillModal.tsx
@@ -5,57 +5,51 @@ import { ISkill } from '../Skill/skill.interface'
 import styles from './SkillModal.module.scss'
 
 interface SkillModalProps {
-	skill: ISkill | null
-	modalIsOpen: boolean
-	setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  skill: ISkill | null
+  modalIsOpen: boolean
+  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 Modal.setAppElement('#root')
 
-const SkillModal: FC<SkillModalProps> = ({
-	skill,
-	modalIsOpen,
-	setModalIsOpen,
-}) => {
-	const closeModal = () => {
-		setTimeout(() => setModalIsOpen(false), 500)
-		const overlay = document.querySelector(
-			'.ReactModal__Overlay',
-		) as HTMLElement
-		overlay.style.opacity = '0'
-	}
+const SkillModal: FC<SkillModalProps> = ({ skill, modalIsOpen, setModalIsOpen }) => {
+  const closeModal = () => {
+    setTimeout(() => setModalIsOpen(false), 500)
+    const overlay = document.querySelector('.ReactModal__Overlay') as HTMLElement
+    overlay.style.opacity = '0'
+  }
 
-	useEffect(() => {
-		if (!modalIsOpen) document.body.style.overflow = 'visible'
-		else document.body.style.overflow = 'hidden'
-	}, [modalIsOpen])
+  useEffect(() => {
+    if (!modalIsOpen) document.body.style.overflow = 'visible'
+    else document.body.style.overflow = 'hidden'
+  }, [modalIsOpen])
 
-	if (!skill) {
-		return null
-	}
+  if (!skill) {
+    return null
+  }
 
-	return (
-		<Modal
-			isOpen={modalIsOpen}
-			onRequestClose={closeModal}
-			contentLabel='Example Modal'
-			overlayClassName={styles.overlay}
-			className={styles.modal}
-		>
-			<div className={styles.container}>
-				<div className={styles.header}>{skill.title}</div>
-				<div className={styles.main}>
-					<ul>
-						{skill.extra?.map(item => (
-							<li key={item}>{item}</li>
-						))}
-					</ul>
-					<div className={styles.separator}></div>
-				</div>
-				<RxCross1 className={styles.cross} onClick={closeModal} />
-			</div>
-		</Modal>
-	)
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel='Example Modal'
+      overlayClassName={styles.overlay}
+      className={styles.modal}
+    >
+      <div className={styles.container}>
+        <div className={styles.header}>{skill.title}</div>
+        <div className={styles.main}>
+          <ul>
+            {skill.extra?.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <div className={styles.separator}></div>
+        </div>
+        <RxCross1 className={styles.cross} onClick={closeModal} />
+      </div>
+    </Modal>
+  )
 }
 
 export default SkillModal
